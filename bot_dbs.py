52c6687b70b725b1b6b28dd2733888bf0ff8730b
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-12-13 13:03:12 +0530

    Formatting the database

--- bot_dbs.py
+++ bot_dbs.py
@@ -15,5 +15,40 @@ def create_table():
     comment TEXT, subreddit TEXT, unix INT, score INT""")
 
 
+def format_data(data):
+    data = data.replace("\n", " newlinechar ").replace(
+        "\r", " newlinechar ").replace('"', "'")
+
+    return data
+
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(
+            pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+
+    except Exception as e:
+        print("find parent : ", e)
+        return False
+
+
 if __name__ == "__main__":
     create_table()
+    row_counter = 0
+    paired_rows = 0
+
+    with open("/RC_{}".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_ytc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
