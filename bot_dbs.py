7195326bd7aac9f1a6e1cf3b84f104d2ac1b1fa3
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-12-16 19:08:20 +0530

    Update database building

--- bot_dbs.py
+++ bot_dbs.py
@@ -66,16 +66,73 @@ def find_parent(pid):
         return False
 
 
+def transaction_bldr(sql):
+
+    global sql_transaction
+    sql_transaction.append(sql)
+
+    if len(sql_transaction) > 1000:
+
+        c.execute('BEGIN TRANSACTION')
+
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+            except:
+                pass
+
+        connection.commit()
+
+        sql_transaction = []
+
+
+def sql_insert_replace_comment(commentid, parentid, parent, comment, subreddit, time, score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(
+            parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+
+        transaction_bldr(sql)
+
+    except Exception as e:
+        print('Replace comment insertion : ', str(e))
+
+
+def sql_insert_has_parent(commentid, parentid, parent, comment, subreddit, time, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(
+            parentid, commentid, parent, comment, subreddit, int(time), score)
+
+        transaction_bldr(sql)
+
+    except Exception as e:
+        print('Has parent insertion : ', str(e))
+
+
+def sql_insert_no_parent(commentid, parentid, comment, subreddit, time, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(
+            parentid, commentid, comment, subreddit, int(time), score)
+
+        transaction_bldr(sql)
+
+    except Exception as e:
+        print('No parent insertion : ', str(e))
+
+
 if __name__ == "__main__":
     create_table()
     row_counter = 0
     paired_rows = 0
 
-    with open("Dataset/{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
+    path = "Dataset/{}/RC_{}"
+
+    with open(path.format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
         for row in f:
+            print(row)
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
+            comment_id = row['name']
             body = format_data(row['body'])
             created_utc = row['created_utc']
             score = row['score']
@@ -99,4 +156,4 @@ if __name__ == "__main__":
 
                         else:
                             sql_insert_no_parent(
-                                comment_id, parent_id, body, subreddit, created_utc)
+                                comment_id, parent_id, body, subreddit, created_utc, score)
