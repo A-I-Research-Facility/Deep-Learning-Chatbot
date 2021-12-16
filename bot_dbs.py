a1df38702e971eb26a5b8bb716a4734450cc2972
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-12-16 13:07:10 +0530

    Determining inset-data-format for database

--- bot_dbs.py
+++ bot_dbs.py
@@ -10,9 +10,7 @@ c = connection.cursor()
 
 
 def create_table():
-    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply
-    (parent_id TEXT PRIMARY_KEY, comment_id TEXT UNIQUE, parent TEXT,
-    comment TEXT, subreddit TEXT, unix INT, score INT""")
+    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY_KEY, comment_id TEXT UNIQUE, parent TEXT,comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
 
 def format_data(data):
@@ -20,6 +18,36 @@ def format_data(data):
         "\r", " newlinechar ").replace('"', "'")
 
     return data
+
+
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(
+            pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+
+    except Exception as e:
+        print("find parent : ", e)
+        return False
+
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+
+    elif len(data) > 1000:
+        return False
+
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+
+    else:
+        return True
 
 
 def find_parent(pid):
@@ -43,7 +71,7 @@ if __name__ == "__main__":
     row_counter = 0
     paired_rows = 0
 
-    with open("/RC_{}".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+    with open("Dataset/{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
         for row in f:
             row_counter += 1
             row = json.loads(row)
@@ -52,3 +80,10 @@ if __name__ == "__main__":
             created_ytc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+
+            if score >= 5:
+                existing_comment_score = find_existing_score(parent_id)
+
+                if existing_comment_score:
+                    if score > existing_comment_score:
